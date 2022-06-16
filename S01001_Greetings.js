21768af3b4ed8726a496b9b6dc548f7c966eafd7
MATHI <76192299+mathisudar@users.noreply.github.com>
2022-06-16 23:26:09 +0530

    Create S01001_Greetings.js

---
+++ S01001_Greetings.js
@@ -0,0 +1,60 @@
+******************************************************
+
+Guten Morgen!
+Guten Morgen!
+Wie heißen Sie?
+Mein Name ist Mathi.
+Wie heißen Sie?
+Mein Name ist Clara. Schön, Sie kennenzulernen.
+Schön, dich kennenzulernen.
+
+Good morning!
+Good morning!
+What's your name?
+My name is Mathi
+What's your name?
+My name is Clara.Nice to meet you.
+Nice to meet you.
+
+
+*******************************************************
+
+
+Guten Abend!
+Guten Abend!
+Wie geht es Ihnen?
+Mir geht es sehr gut und dir?
+Mir geht es sehr gut danke!
+
+
+
+Good evening!
+Good evening!
+How are you doing?
+I'm very well and you?
+I am very well thank you!
+
+*********************************************************
+
+Guten Morgen!
+Guten Morgen!
+Mein Name ist Mathi. Wie heißen Sie?
+Mein Name ist Clara. Schön, Sie kennenzulernen.
+Schön, dich kennenzulernen. Wie geht es dir?
+Mir geht es gut und dir?
+Mir geht es gut.
+Bis bald
+Auf Wiedersehen
+
+Good morning!
+Good morning!
+My name is Mathi. What's your name?
+My name is Clara.Nice to meet you.
+Nice to meet you. How are you?
+I'm doing well, and you?
+I'm doing well.
+See you soon
+Goodbye
+
+*********************************************************
+
